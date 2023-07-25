a3971484a7b53639e96262fe15d45d4694b4935b
leocanto <leonardo.canto@edu.pucrs.br>
2023-07-25 16:54:02 -0300

    page login: functions, routes and create dashboard page

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,34 @@
+export default function DashboardPage() {
+    return <>
+        <section className="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5 antialiased">
+
+            <div className="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
+                <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
+                    <div className="w-full md:w-1/2">
+                        <form className="flex items-center">
+                            <div className="relative w-full">
+                                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
+                        <button type="button" id="createProductModalButton" data-modal-target="createProductModal" data-modal-toggle="createProductModal" className="flex items-center justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
+
+                        </button>
+                        <div className="flex items-center space-x-3 w-full md:w-auto">
+                            <button className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+
+                            </button>
+                           
+                            <button className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                            </button>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>
+}
---
+++ frontend/src/constants/routes.ts
@@ -0,0 +1,10 @@
+export const ROUTES = {
+    private: {
+        dashboard: {
+            name: 'dashboard',
+        }
+    },
+    public: {
+        login: '/',
+    },
+}
