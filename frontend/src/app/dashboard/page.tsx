c1e57692e867c8e4ab636b40fd0e31a2454b6cc0
leocanto <leonardo.canto@edu.pucrs.br>
2023-07-31 23:33:19 -0300

    frontend: forms dashboard / ~reponsive

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,34 +1,39 @@
 export default function DashboardPage() {
     return <>
-        <section className="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5 antialiased">
-
-            <div className="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
-                <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
-                    <div className="w-full md:w-1/2">
-                        <form className="flex items-center">
-                            <div className="relative w-full">
-                                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-
-                                </div>
-                            </div>
-                        </form>
+        <div className="bg-gradient-to-r from-black p-10 to-blue-500 flex items-center justify-center">
+            <div className="bg-gray-100 dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden flex md:flex-row justify-between">
+                <form className="flex items-start justify-start md:space-x-1 p-4">
+                    <div className="relative w-full">
+                        <label>Nome</label>
+                        <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                              focus:border-primary-500 block h-10 w-full p-2 dark:bg-gray-700 dark:border-gray-600
+                               dark:text-white  dark:focus:border-primary-500" placeholder="Nome" required />
                     </div>
-                    <div className="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
-                        <button type="button" id="createProductModalButton" data-modal-target="createProductModal" data-modal-toggle="createProductModal" className="flex items-center justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
-
-                        </button>
-                        <div className="flex items-center space-x-3 w-full md:w-auto">
-                            <button className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-
-                            </button>
-                           
-                            <button className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                            </button>
-                            
-                        </div>
+                    <div className="relative w-full">
+                        <label>E-mail</label>
+                        <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                              focus:border-primary-500 block h-10 w-full p-2 dark:bg-gray-700 dark:border-gray-600
+                               dark:text-white  dark:focus:border-primary-500" placeholder="Email" required />
                     </div>
-                </div>
+                    <div className="relative w-full">
+                        <label>Telefone</label>
+                        <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                              focus:border-primary-500 block h-10 w-full p-2 dark:bg-gray-700 dark:border-gray-600
+                               dark:text-white  dark:focus:border-primary-500" placeholder="Telefone" required />
+                    </div>
+                    <div className=" w-full">
+                        <label>Data de nascimento</label>
+                        <input type="date" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                              focus:border-primary-500 block w-full h-10 p-2 dark:bg-gray-700 dark:border-gray-600
+                               dark:text-white  dark:focus:border-primary-500" placeholder="Date" required />
+
+                    </div>
+                    <button className="my-6 h-10 bg-gradient-to-r from-black to-blue-500 hover:from-blue-800 hover:to-blue-500 text-white font-bold py-1.5 px-6 rounded focus:outline-none focus:shadow-outline"
+                        type="submit">
+                        Save
+                    </button>
+                </form>
             </div>
-        </section>
+        </div>
     </>
 }
