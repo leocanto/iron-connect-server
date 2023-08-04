903065b8ab34491b104e30e00394090801fb645a
leocanto <leonardo.canto@edu.pucrs.br>
2023-08-04 14:33:13 -0300

    database feat: client models -> meal/food/training

--- backend/src/models/client.js
+++ backend/src/models/client.js
@@ -2,9 +2,24 @@ import mongoose from "mongoose";
 
 const ClientSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    //start_date: { type: Date, required: true},
-    //birth_date: { type: Date, required: true },
-    phone: { type: String, required: true},
+    birth_date: { type: Date, required: true },
+    phone: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    meal: [{
+        description: { type: String, required: true },
+        time: { type: String, required: true },
+        foods: [{
+            name: { type: String, required: true },
+            amount: { type: String, required: true },
+            desciption: { type: String },
+        },],
+    }],
+    training: [{
+        name: { type: String, required: true },
+        series: { type: Number, required: true },
+        repetitions: { type: Number, required: true },
+        description: { type: String },
+    }],
 })
 
 export default mongoose.models.Client || mongoose.model('Client', ClientSchema)
