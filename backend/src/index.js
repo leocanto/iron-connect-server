3e197bd7aa68013cacfcbe1ad3c6b2cca685c4ed
leocanto <leonardo.canto@edu.pucrs.br>
2023-08-06 16:31:15 -0300

    fix: routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
-import clientController from "./controllers/client.js";
-import adminController from "./controllers/admin.js";
+import clientController from "./controllers/clientController.js";
+import adminController from "./controllers/adminController.js";
+import mealController from "./controllers/mealController.js";
+import trainingController from "./controllers/trainingController.js";
 import cors from "cors";
 import * as dotenv from "dotenv";
 
@@ -14,8 +16,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/admin", adminController);
 app.use("/client", clientController);
+app.use("/admin", adminController);
+app.use("/meal", mealController);
+app.use("/training", trainingController);
 
 
 app.listen(PORT, HOST);
